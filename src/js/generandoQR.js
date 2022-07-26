f20185eccadb91d2e607403c4141648723070da5
Thomas Pufahl <thomas.r.pufahl@gmail.com>
2022-07-26 15:46:15 -0300

    v1.1.0

--- src/js/generandoQR.js
+++ src/js/generandoQR.js
@@ -23,9 +23,12 @@ const QR = new QRCode(contenedorQR, {
 // En la variable 'qrValue' se concatenan los datos de las variables de formulario y organiza estos datos
 //junto a otros valores de tipo string para el correcto funcionamiento dentro del sistema del control de accesos.
 
-// Por ultimo, es tomada la variable QR y se le asigna una propiedad(funcion).
-//La funcion asignada es la encargada de realizar el dibujo QR, 
-//y toma como parametro el dato que se quiere almacenar en el QR.
+// Este procedimiento tambien se encarga del dibujo QR. Para ello, es tomada la variable QR y se le asigna una propiedad(funcion).
+//La funcion asignada es la encargada de realizar el dibujo QR, y toma como parametro el dato que se quiere almacenar en el QR.
+
+// Por ultimo, se crea la variable de 'personaDelQR',
+//esta es una concatenacion de los datos: nombre y apellido, del formulario. Va a ser utilizada para modificar el valor de un span. 
+//El span pasara de un texto vacio a mostrar el nombre de la persona encima del codigo qr.
 formularioQR.addEventListener("submit", (e) => {
     e.preventDefault();
     //variables de formulario
@@ -35,7 +38,11 @@ formularioQR.addEventListener("submit", (e) => {
     const nac = document.querySelector('#nac').value;
     //variable de formato
     const qrValue = '00000000000@' + apellido + '@' + nombre + '@N@' + dni + '@N@' + nac + '@00-00-0000';
-    //asignacion de propiedad
+    //dibuja qr
     QR.makeCode(qrValue);
+    //variable nombre de la persona del qr
+    const personaDelQR = nombre + ' ' +apellido;
+    //modifica span
+    document.getElementById('personaDelQR').innerHTML = personaDelQR;
 });
 
